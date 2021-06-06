c9c55b8a43c695ab5010bbdf1a552f3c142e0520
Leonardo Avendaño <leo.aven94@gmail.com>
2021-06-06 18:27:37 -0300

    Add working login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  myForm:FormGroup;
 
-  ngOnInit(): void {
+  constructor(public fb:FormBuilder) { 
+    this.myForm = this.fb.group({
+      email:[ '', [Validators.required, Validators.email] ],
+      password:[ '', [Validators.required, Validators.minLength(6), Validators.maxLength(10)] ],
+    })
+  }
+
+  ngOnInit(): void {}
+
+  login() {
+    console.log(this.myForm.value)
   }
 
 }
