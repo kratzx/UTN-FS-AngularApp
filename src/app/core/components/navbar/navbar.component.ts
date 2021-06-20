50b3e5925bd8cd3345155bf0776805aa8b2e3a00
Leonardo Avendaño <leo.aven94@gmail.com>
2021-06-20 13:23:28 -0300

    Add logout button

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/firebase/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [AuthService]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+    public auth:AuthService
+  ) {}
 
   ngOnInit(): void {}
 
+  logout(){
+    this.auth.logout();
+  }
 }
