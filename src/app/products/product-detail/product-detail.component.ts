afafed639f4347fc5012f2a89e10a7be90ecf40b
Leonardo Avendaño <leo.aven94@gmail.com>
2021-06-20 15:26:37 -0300

    Add productList and productDetail

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FirebaseItemsService } from 'src/app/core/services/firebase/firebase-items.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  providers: [FirebaseItemsService]
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  product: any;
+  state: any;
+
+  constructor(
+    public db: FirebaseItemsService,
+    public route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.state = 'loading';
+    this.route.params
+      .subscribe( (params) => {
+        const id = params['id'].toString();
+        this.db.getProduct(id).then((product) => {
+          this.state = 'ready';
+          this.product = product;
+        });
+      })
   }
 
 }
--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseItemsService } from '../../core/services/firebase/firebase-items.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  styleUrls: ['./products-list.component.css'],
+  providers: [FirebaseItemsService]
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  products:any[];
+  state:any;
+
+  constructor(
+    public db: FirebaseItemsService
+  ) {}
 
   ngOnInit(): void {
+    this.state = 'loading';
+    this.db.getProducts().then((products) => {
+      this.state = 'ready';
+      this.products = products;
+    })
   }
-
 }
